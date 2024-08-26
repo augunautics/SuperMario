39de255994b13faa000aa57a571dd49a4dcc37fe
augunautics <thomas.a.augustine@gmail.com>
2024-08-26 15:41:43 -0700

    done

--- js/GameConfig.js
+++ js/GameConfig.js
@@ -7,13 +7,13 @@ export default class GameConfig {
 
     static backgroundData = {
         name: 'world1-1',
-        position: { x: 0, y: 0, width: 256, height: 240 }, // Position of the background image
-        viewport: { x: 0, y: 0, width: 512, height: 480 }, // Viewport dimensions
+        position: { x: 0, y: 0, width: 256, height: 240-32 }, // Position of the background image
+        viewport: { x: 0, y: 0, width: 512, height: 480-32 }, // Viewport dimensions
     };
 
     static marioData = {
         name: 'mario',
-        position: { x: 400, y: 380, width: 32, height: 32 }, // Mario's position and size
+        position: { x: 400, y: 816, width: 32, height: 32 }, // Mario's position and size
         crop: { x: 0, y: 8, width: 16, height: 16 }, // Crop settings for Mario's sprite
     };
 
